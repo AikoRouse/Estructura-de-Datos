f63aa472f125af9655c8543446fb6f571da2bd40
Camila A Peralta R <112417026+AikoRouse@users.noreply.github.com>
2023-04-20 12:17:00 -0300

    Add files via upload

---
+++ Estructura-de-Datos/pryEstructuraDeDatos/pryEstructuraDeDatos/frmEstruct.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEstructuraDeDatos
+{
+    public partial class frmEstruct : Form
+    {
+        public frmEstruct()
+        {
+            InitializeComponent();
+        }
+
+        private void datosDelProgramadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmDatos frmDatos = new frmDatos();
+            frmDatos.Show();
+        }
+
+        private void pilaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmPila frmPila = new frmPila();
+            frmPila.Show();
+        }
+
+        private void colaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCola frmCola = new frmCola();
+            frmCola.Show();
+        }
+
+        private void estructurasLinealesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmListaSimple frmListaSimple = new frmListaSimple();   
+            frmListaSimple.Show();
+        }
+    }
+}
---
+++ Estructura-de-Datos/pryEstructuraDeDatos/pryEstructuraDeDatos/frmListaSimple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryEstructuraDeDatos
+{
+    public partial class frmListaSimple : Form
+    {
+        public frmListaSimple()
+        {
+            InitializeComponent();
+        }
+
+        clsListaSimple ListaSimple = new clsListaSimple();
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+            clsNodo ObjNodo = new clsNodo();
+            ObjNodo.Codigo = Convert.ToInt32(txtCodigo.Text);
+            ObjNodo.Nombre = txtNombre.Text;
+            ObjNodo.Tramite = txtTramite.Text;
+
+            ListaSimple.Agregar(ObjNodo);
+            ListaSimple.Recorrer(dgvLista);
+            ListaSimple.Recorrer(lstListaSimple);
+            txtCodigo.Text = "";
+            txtNombre.Text = "";
+            txtTramite.Text = "";
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lblCodigoE_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
